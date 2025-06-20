2255f2ada7530c17dae991c8403ce9927a268d25
Tristan Cacqueray <tdecacqu@redhat.com>
2025-06-20 09:37:23 +0000

    MD2Jira: always update the document
    This change ensures that created story IDs are written to the file.

--- md2jira/app/Main.hs
+++ md2jira/app/Main.hs
@@ -1,12 +1,12 @@
 -- | A CLI interface for md2jira
 module Main (main) where
 
-import Control.Monad (void, when)
+import Control.Monad (when)
 import Data.Aeson (eitherDecodeFileStrict, encodeFile)
 import Data.ByteString.Char8 qualified as BS
 import Data.Text qualified as T
 import Data.Text.IO qualified as T
-import Jira (newJiraClient)
+import Jira (Board (..), newJiraClient)
 import MD2Jira (eval, parse, printer)
 import Main.Utf8 (withUtf8)
 import Network.HTTP.Client.TLS (newTlsManager)
@@ -24,34 +24,36 @@ mainUtf =
         ["--help"] -> die "usage: md2jira FILE"
         [fp] ->
             fmap parse (T.readFile fp) >>= \case
-                Right doc -> T.writeFile fp =<< go doc
+                Right doc -> go doc $ T.writeFile fp
                 Left err -> die $ T.pack fp <> ": parse error: " <> err
         [] ->
             fmap parse T.getContents >>= \case
-                Right doc -> void $ go doc
+                Right doc -> go doc T.putStrLn
                 Left err -> die $ "Could not parse input: " <> err
         _ -> die "usage: md2jira < FILE"
   where
-    go doc = do
+    go doc updateDocument = do
         project <- T.pack <$> getEnv "JIRA_PROJECT"
-        mBoard <- fmap Jira.Board <$> getEnv "JIRA_BOARD"
+        mBoard <- fmap (Jira.Board . read) <$> lookupEnv "JIRA_BOARD"
         cachePath <- getCachePath
         client <- mkClient
         cache <- loadCache cachePath
         let logger = T.putStrLn
         (newDoc, newCache, errors) <- eval logger client project mBoard doc cache
         let updated = cache /= newCache
-        when updated $ encodeFile cachePath newCache
+        when updated do
+            encodeFile cachePath newCache
+            updateDocument $ printer newDoc
         case errors of
             []
-                | updated -> pure $ printer newDoc
+                | updated -> pure ()
                 | otherwise -> die "Already synced"
             xs -> die $ T.unlines xs
 
     mkClient = do
         url <- T.pack <$> getEnv "JIRA_URL"
         token <- BS.pack <$> getEnv "JIRA_TOKEN"
-        Jira.newJiraClient url Nothing token <$> newTlsManager
+        Jira.newJiraClient url Nothing Nothing token <$> newTlsManager
 
     loadCache path =
         doesPathExist path >>= \case
